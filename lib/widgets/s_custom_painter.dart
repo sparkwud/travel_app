1b65d0c72d20a03253fca57a6e80c996ade3842b
Kelvin Agbenyo <kelvinagbenyo@gmail.com>
2024-09-11 10:35:00 +0000

    creae cutout for destination card

---
+++ lib/widgets/s_custom_painter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_palette.dart';
+
+class SCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final calc = (10 / 15) * size.width;
+    Path path_0 = Path();
+    path_0.moveTo(size.width, 0);
+    path_0.cubicTo(size.width, calc, calc, size.width, 0, size.width);
+    path_0.lineTo(size.width, size.height);
+    path_0.lineTo(size.width, 0);
+
+    final Paint paint0Fill = Paint()..style = PaintingStyle.fill;
+    paint0Fill.color = AppPalette.offWhite;
+    canvas.drawPath(path_0, paint0Fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
